ae316cbdef5b17aac6f9041ca6f34ab7366aab38
kieusign <kieusign12@gmail.com>
2019-06-08 02:46:33 +0700

    exercise 2 for week 2

---
+++ ScrollApp/app/src/main/java/com/scrollapp/scrollapp/ScrollActivity.kt
@@ -0,0 +1,12 @@
+package com.scrollapp.scrollapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scroll)
+    }
+}
